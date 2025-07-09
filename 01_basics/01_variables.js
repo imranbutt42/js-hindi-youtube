c07768ede91a1ca3d351698b163514881ca45460
imranbutt42 <ibutt8662@gmail.com>
2025-07-09 11:29:43 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountid = 269
+let accountemail = "imran69@gmail.com"
+var accountCity = "lahore"
+accountpassword = "6969"
+
+let accountstate;
+accountstate;
+accountstate;
+console.log(accountpassword);
+console.table([accountpassword,accountid,accountemail,accountstate])

cd6dd93afbed470eb4290880c2fb659c2f5c51f9
imranbutt42 <ibutt8662@gmail.com>
2025-07-09 11:58:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+" use strict"; //use latest features of JS
+// alert (3 + 3 ) can be used on browser not in node.js
+
+console.log(3+3) 
+console.log("imran") 
+
+let name= "imran"
+let age = 20
+let isloggedin= false
+
+/*
+
+
